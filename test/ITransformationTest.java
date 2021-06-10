4597ea09c7bfd01a4257e6e8331f42b48a6598bd
benonivainqueur <57690186+benonivainqueur@users.noreply.github.com>
2021-06-09 21:27:21 -0500

    Added functionality and tests. Added .equals() methods and model implementation.

---
+++ test/ITransformationTest.java
@@ -0,0 +1,18 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import cs3500.imageProcessing.model.IPixel;
+import cs3500.imageProcessing.model.IPixelImage;
+import cs3500.imageProcessing.model.Pixel;
+import cs3500.imageProcessing.model.PixelImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ITransformationTest {
+
+}
