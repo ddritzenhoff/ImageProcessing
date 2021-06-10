ad875dbb2dbf9b790583f78b04d9439cba9c2f66
benonivainqueur <57690186+benonivainqueur@users.noreply.github.com>
2021-06-09 19:46:01 -0500

    Tests complete for IPixelTest.

--- test/IPixelImageTest.java
+++ test/IPixelImageTest.java
@@ -1,3 +1,38 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import cs3500.imageProcessing.model.IPixel;
+import cs3500.imageProcessing.model.Pixel;
+import org.junit.Before;
+import org.junit.Test;
+
 public class IPixelImageTest {
+
+
+  @Before
+  public void init() {
+  }
+
+  @Test
+  public void getPixels() {
+  }
+
+  @Test
+  public void getPixel() {
+  }
+
+  @Test
+  public void render() {
+  }
+
+  @Test
+  public void getNumRows() {
+  }
+
+  @Test
+  public void getNumPixelsInRow() {
+  }
+
 
 }
